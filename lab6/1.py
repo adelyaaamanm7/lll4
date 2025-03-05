a009c549e8e8602d12787211e6d6422de039f40b
adelyaaamanm7 <adelyaaman3@gmail.com>
2025-03-05 12:35:26 +0500

    Delete lab6 directory

--- lab6/1.py
+++
@@ -1,6 +0,0 @@
-from math import prod
-def multiply_list(nums):
-    return prod(nums) 
-numbers = list(map(int, input().split()))
-result = multiply_list(numbers)
-print(result)
--- lab6/2.py
+++
@@ -1,9 +0,0 @@
-def count_upper_lower(s):
-    upper_count = sum(1 for char in s if char.isupper())
-    lower_count = sum(1 for char in s if char.islower())
-    
-    print(f"Uppercase letters: {upper_count}")
-    print(f"Lowercase letters: {lower_count}")
-
-user_input = input("Enter a string: ")
-count_upper_lower(user_input)
